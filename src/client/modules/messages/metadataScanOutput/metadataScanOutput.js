671d1955027a25dede92feac8eec1aa8f47336ea
jhaydraude <jhaydraude@salesforce.com>
2020-09-09 13:31:01 -0400

    Monitored Inventory UI

--- src/client/modules/messages/metadataScanOutput/metadataScanOutput.js
+++ src/client/modules/messages/metadataScanOutput/metadataScanOutput.js
@@ -1,5 +1,11 @@
 import { LightningElement, api } from 'lwc';
 
+const monitoredInvColumns = [
+  {label: 'Metadata Type', fieldName: 'label', type: 'text'},
+  {label: 'Count', fieldName: 'count', type: 'number'}
+];
+
+
 export default class MetadataScanOutput extends LightningElement {
   @api result;
 
